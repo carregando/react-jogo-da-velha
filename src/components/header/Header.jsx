6cb3e0dfb11dc98230f26cbf70a0a1a1e66311b8
Humberto Ferreira Lemos <joojnow@gmail.com>
2023-07-10 20:06:22 -0400

    Criado componente de icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import styles from './Header.module.css'
 
 import Title from '../title/Title'
 import Subtitle from '../subtitle/Subtitle'
+import Icon from '../icon/Icon'
 
 function Header () {
   return (
@@ -9,6 +10,9 @@ function Header () {
      <div className = {styles.header}>
        <Title>Jogo da Velha</Title>
        <Subtitle>Criado por Humberto F. Lemos</Subtitle>
+       <div className={styles.iconContent}>
+         <Icon iconName="github.svg" size="40px" link="https://github.com/carregando"/>
+       </div>
      </div>
     </>
   )
