1c7bd761bae9d78d140203bd44a815bd24335a9f
Humberto Ferreira Lemos <joojnow@gmail.com>
2023-07-13 18:12:49 -0400

    Criado status no GameOption

--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,8 +1,17 @@
 import styles from '../gameOption/GameOption.module.css'
+import Icon from '../icon/Icon'
 
-function GameOption () {
+const GameIcon = ({ iconName }) => <Icon iconName= {iconName} size = "25px"/>
+
+function GameOption ({ status }) {
   return (
     <div className = {styles.gameOption}>
+      {
+        status === 1 && <GameIcon iconName ='circle.svg'/>
+      }
+      {
+        status === -1 && <GameIcon iconName ='x.svg'/>
+      }
     </div>
   )
 }
