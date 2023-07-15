d6f335a3f430e555489ea7a51111b4ba07448c4d
Humberto Ferreira Lemos <joojnow@gmail.com>
2023-07-15 01:16:24 -0400

    Criado Lógica do jogo

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,23 +1,46 @@
 import { useState } from 'react'
 
+import Icon from '../icon/Icon'
 import styles from '../game/Game.module.css'
 import GameOption from '../gameOption/GameOption'
 
 
 function Game () {
   const [gameState, setGameState] = useState(Array(9).fill(0))
+  const [currentPlayer, setCurrentPlayer] = useState(1)
+
+  const handleClick = (pos) => {
+    if (gameState[pos] === 0) {
+      let newGameState = [...gameState]
+     newGameState[pos] = currentPlayer
+     setCurrentPlayer(currentPlayer * -1)
+     setGameState(newGameState)
+    }
+  }
   return (
-     <div className = {styles.game}>
+    <div className={styles.gameContent}>
+      <div className = {styles.game}>
       {
         gameState.map((value, pos) =>
         <GameOption 
           key={`game-option-pos-${pos}`} 
           status={value}
+          onClick={() => handleClick(pos)}
         /> 
         ) 
       }
      </div>
+     <div className= {styles.gameInfo}>
+      <h4>Próximo a jogar</h4>
+      {
+        currentPlayer === 1 && <Icon iconName ='circle.svg' />
+      }
+      {
+        currentPlayer === -1 && <Icon iconName ='x.svg' />
+      }
+     </div>
+    </div>
   )
 }
 
-export default Game
+export default Game 
