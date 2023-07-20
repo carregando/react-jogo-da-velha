f94eb385fb9c8dbeb91bad4942dadfa94abf6954
Humberto Ferreira Lemos <joojnow@gmail.com>
2023-07-20 12:35:14 -0400

    Criado script de build e start

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,14 @@ import Gamepage from '../src/components/gamePage/GamePage'
 function HomePage () {
   
   return (
+    <>
+    <head>
+      <title>Jogo da Velha</title>
+    </head>
     <div>
      <Gamepage/>
     </div>
+    </>
   )
 }
 
